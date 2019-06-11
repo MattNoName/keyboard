6cd6c74f2acf3a8095c22cfb0de7a494c77261bb
Matt <31932179+MattNoName@users.noreply.github.com>
2019-06-11 16:14:43 -0400

    Updated home page, app's view, and .jar

--- keyboard/ViewModel.java
+++ keyboard/ViewModel.java
@@ -10,17 +10,23 @@ public class ViewModel {
     String getThankYouText(){
         StringBuilder strBuilder=new StringBuilder();
         strBuilder.append("Thank you to Osa for introducing me to the Java midi sound library whioch is used to create sound with your computer.\n\n");
-        strBuilder.append("Thank you to Osa again and to Vanely for encouraging me to create this keyboard.");
+        
+        strBuilder.append("Thank you to Osa again and to Vanely for encouraging me to create this keyboard.\n\n");
+        
+        strBuilder.append("Thank you to Wayne for testing it on Windows so we could find why it didn't initially work on Windows.\n\n");
+        
+        strBuilder.append("Thank you to Tsung-Lin for testing it on Windows again once it was made compatible for Windows.");
         return strBuilder.toString();
     }
     
     String getTeachingText(boolean spoken){
         StringBuilder strBuilder=new StringBuilder();
-        strBuilder.append("This keyboard is an electric keyboard that plays the notes as you play the keys.\n\n");
+        strBuilder.append("This app lets your computer keyboard act as an electric keyboard that plays the notes as you play the keys.\n\n");
         
-        strBuilder.append("Its window must be the last thing you clicked on for it to play the notes you play.\n\n");
+        strBuilder.append("Its window must be the last thing you clicked on for it to play the notes you play. ");
+        strBuilder.append("No keyboard will show up on your screen, and the window of this application must have focus (such as by being the last thing you clicked on) in order for it to play notes when you play the keys.\n\n");
         
-        strBuilder.append("It is really meant for a QWERTY keyboard (a keyboard where the top right letters are, from left to right, Q, W, E, R T, Y.  ");
+        strBuilder.append("It is really meant for a QWERTY keyboard (a keyboard where the top left letters are, from left to right, Q, W, E, R T, Y).  ");
         strBuilder.append("This is important becuase you want the keys for the notes you want to be where you want them to be.  ");
         strBuilder.append("All the keys for notes are within the two rows of keys that are below the number keys.\n\n");
         
@@ -28,23 +34,23 @@ public class ViewModel {
         strBuilder.append("To use the music theory keyboard, type M.  ");
         strBuilder.append("To use the piano keyboard, type N.\n\n");
         
+        strBuilder.append("For the piano keyboard, the keys are like those on an electric keyboard or a piano.  ");
+        strBuilder.append("The first key on your keyboard of the key you are using is A or Q for all keys.\n\n");
+        
         strBuilder.append("The music theory keyboard has each note one key away from the next on the keyboard.  ");
         strBuilder.append("Go up and to the right or down and to the left to go up a note.  ");
         strBuilder.append("Go down and to the left, or up and to the left to go down a note.\n\n");
         
-        strBuilder.append("For the piano keyboard, the keys are like those on an electric keyboard or a piano.  ");
-        strBuilder.append("The first key on your keyboard of the key you are using is A or Q for all keys.  ");
-        
         strBuilder.append("For either keyboard, switch octaves by pressing Z, X, C, V, or B.  ");
         strBuilder.append("C gives you a keyboard that uses middle C.  ");
         strBuilder.append("X is down one octave from middle C and Z is down two.  ");
-        strBuilder.append("V is up pone octave from middle C and B is up two.\n\n");
+        strBuilder.append("V is up one octave from middle C and B is up two.\n\n");
         
         strBuilder.append("To change keys, use the number keys, the minus key, and the equals key.  ");
         if(spoken){
             strBuilder.append("1 is for C, 2 is for C sharp, 3 is for D, 4 is for D sharp, 5 is for E, 6 is for F, 7 is for F sharp, 8 is for G, 9 is for G sharp, 0 is for A, minus is for B sharp, and equals is for B.\n\n");
         }else{
-            strBuilder.append("1 is for C, 2 is for C# (C sharp), 3 is for D, 4 is for D# (D sharp), 5 is for E, 6 is for F, 7 is for F# (F sharp), 8 is for G, 9 is for G# (G sharp), 0 is for A, minus is for A# (B sharp), and equals is for B.\n\n");
+            strBuilder.append("1 is for C, 2 is for C# (C sharp), 3 is for D, 4 is for D# (D sharp), 5 is for E, 6 is for F, 7 is for F# (F sharp), 8 is for G, 9 is for G# (G sharp), 0 is for A, minus is for A# (A sharp), and equals is for B.\n\n");
         }
         strBuilder.append("To make notes keep playiong after you lift your fingers, press caps lock.  ");
         strBuilder.append("To stop this, press caps lock again.  ");
